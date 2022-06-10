2386bfdd503a74dd57b52a86a0a8b54283c2677f
Sebastian Apitz <apitz_sebastian@web.de>
2022-06-10 06:01:47 +0200

    added script for trades dump

--- data/rb-dump.sh
+++ data/rb-dump.sh
@@ -5,5 +5,5 @@ rm -f rb-announcements.json;
 rm -f rb-corporates.json;
 
 elasticdump --input=http://localhost:9200/rb-persons --output=rb-persons.json --type=data;
-elasticdump --input=http://localhost:9200/rb-announcements.json --output=rb-announcements.json.json --type=data;
-elasticdump --input=http://localhost:9200/rb-corporates.json --output=rb-corporates.json.json --type=data;
+elasticdump --input=http://localhost:9200/rb-announcements --output=rb-announcements.json --type=data;
+elasticdump --input=http://localhost:9200/rb-corporates --output=rb-corporates.json --type=data;
