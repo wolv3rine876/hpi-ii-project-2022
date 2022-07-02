526e4ee1593ffb0ec69c8e0eb901397fc5180eb8
Sebastian Apitz <apitz_sebastian@web.de>
2022-07-02 17:47:25 +0200

    removed old dumps1

--- data/rb-dump.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rm -f rb-persons.json;
-rm -f rb-announcements.json;
-rm -f rb-corporates.json;
-
-elasticdump --input=http://localhost:9200/rb-persons --output=rb-persons.json --type=data;
-elasticdump --input=http://localhost:9200/rb-announcements --output=rb-announcements.json --type=data;
-elasticdump --input=http://localhost:9200/rb-corporates --output=rb-corporates.json --type=data;
